63b53dd0895720b3cbe2fcded06d1e60c2a1e1ae
defytek <haroldmunoz90@gmail.com>
2017-06-16 18:29:34 -0500

    Header fragment added Resources: backg = background de la barra de experiencia

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/MainActivity.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
+
         circleMenu = (CircleMenu) findViewById(R.id.circle_menu);
         circleMenu.setMainMenu(Color.parseColor("#CDCDCD"),R.drawable.albert,R.drawable.albert).addSubMenu(Color.parseColor("#258CFF"),R.drawable.practicar).
                 addSubMenu(Color.parseColor("#6d4c41"),R.drawable.study).addSubMenu(Color.parseColor("#FF0000"),R.drawable.challenge)
