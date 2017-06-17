638774f3299070a1d934976e69d61490e09111d9
Didier <deramirez19@misena.edu.co>
2017-06-16 19:40:13 -0500

    creacion de paquetes

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/test/shop/ShopActivity.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/test/shop/ShopActivity.java
@@ -1,4 +1,4 @@
-package olimpiadas.sena.com.olimpiadasmath.shop;
+package olimpiadas.sena.com.olimpiadasmath.activities.test.shop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
