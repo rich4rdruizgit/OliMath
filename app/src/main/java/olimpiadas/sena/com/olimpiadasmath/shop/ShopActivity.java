3d6b7d08f8271e1af6b99263319d6ff1abcd7210
Didier <deramirez19@misena.edu.co>
2017-06-16 19:26:25 -0500

    creacion de paquetes

---
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/shop/ShopActivity.java
@@ -0,0 +1,15 @@
+package olimpiadas.sena.com.olimpiadasmath.shop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import olimpiadas.sena.com.olimpiadasmath.R;
+
+public class ShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
