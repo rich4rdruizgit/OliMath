d539ea00d7a3d0a76a1634f1cfaa5ad969653746
rich4rd <rich4rdruiz@gmail.com>
2017-06-16 23:13:54 -0500

    Commit tienda implementado

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/menu/MainActivity.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/menu/MainActivity.java
@@ -1,17 +1,24 @@
-package olimpiadas.sena.com.olimpiadasmath;
+package olimpiadas.sena.com.olimpiadasmath.activities.menu;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
 
-public class MainActivity extends AppCompatActivity {
+import olimpiadas.sena.com.olimpiadasmath.R;
+import olimpiadas.sena.com.olimpiadasmath.activities.shop.ShopActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     String arrayName[] = {"Practice","Study","Challenge"};
     CircleMenu circleMenu;
+    Button btnShop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +27,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+        btnShop = (Button) findViewById(R.id.btn_menu_shop);
+        btnShop.setOnClickListener(this);
 
         circleMenu = (CircleMenu) findViewById(R.id.circle_menu);
-
         circleMenu.setMainMenu(Color.parseColor("#CDCDCD"),R.drawable.albert,R.drawable.albert).addSubMenu(Color.parseColor("#258CFF"),R.drawable.practicar).
                 addSubMenu(Color.parseColor("#6d4c41"),R.drawable.study).addSubMenu(Color.parseColor("#FF0000"),R.drawable.challenge)
                 .setOnMenuSelectedListener(new OnMenuSelectedListener() {
@@ -31,5 +39,17 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "Seleccionaste "+ arrayName[i], Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_menu_shop:
+                Intent intent = new Intent(MainActivity.this, ShopActivity.class);
+                startActivity(intent);
+            break;
+            default:
+                break;
+        }
     }
 }
