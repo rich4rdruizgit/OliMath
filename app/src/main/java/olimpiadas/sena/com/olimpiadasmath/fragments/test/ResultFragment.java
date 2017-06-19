70687bb0d8ed74543a26faf334c04d037a6d7e29
Didier <deramirez19@misena.edu.co>
2017-06-19 17:53:04 -0500

    dimens

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/fragments/test/ResultFragment.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/fragments/test/ResultFragment.java
@@ -55,6 +55,7 @@ public class ResultFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -64,6 +65,7 @@ public class ResultFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false);
     }
