a1968e2ab213363a682f4c25805e76ae9740fc3a
Didier <mcabrerapatino@gmail.com>
2017-06-19 19:32:55 -0500

    Se agrega fuente

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/profile/ProfileActivity.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/profile/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import olimpiadas.sena.com.olimpiadasmath.R;
+import olimpiadas.sena.com.olimpiadasmath.librerias.identicons.Identicon;
 
 public class ProfileActivity extends AppCompatActivity {
     TextView txt_level, txt_levelnum;
@@ -15,6 +16,15 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        ((TextView)findViewById(R.id.textView_level)).setTypeface(Typeface.createFromAsset(this.getAssets(),"grobold.ttf"));
+        ((TextView)findViewById(R.id.textView_level)).setTypeface(Typeface.createFromAsset(this.getAssets(),"grobold.ttf"));
+
+        Identicon identicon = (Identicon) findViewById(R.id.identicon);
+        identicon.show("john.doe@example.org");
+        // identicon.show(myObject);
+        // identicon.show(42);
+        // identicon.show(System.currentTimeMillis());
+        // identicon.show(true);
 
 
     }
