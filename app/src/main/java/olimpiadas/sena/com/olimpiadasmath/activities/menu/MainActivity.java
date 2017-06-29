9da7011045104a42b1012ca4fad7e1f3cacae885
Carlos Andres Chapid Inga <cachapid@miena.edu.co>
2017-06-28 21:18:09 -0500

    Carlos Chapid, Actualización del perfil cuando se cambien coins o Tickets, 28/06/2017 9:17 p.m

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/menu/MainActivity.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/menu/MainActivity.java
@@ -166,4 +166,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
+
+
+
 }
