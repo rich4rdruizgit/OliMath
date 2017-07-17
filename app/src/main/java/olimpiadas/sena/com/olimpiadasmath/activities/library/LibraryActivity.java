69636fb74af348cb5119ad31d8d168d4d9ffb0c8
Mile <mcabrerapatino@gmail.com>
2017-07-17 13:43:37 -0500

    Se dispone la Biblioteca en forma de LinearLayoutManager  "LibraryActivity"

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/library/LibraryActivity.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/library/LibraryActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,29 +15,22 @@ import android.widget.ListView;
 import olimpiadas.sena.com.olimpiadasmath.R;
 import olimpiadas.sena.com.olimpiadasmath.adapter.library.TopicAdapter;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
-
+/* Modificado por Mile 17/07/2017*/
 public class LibraryActivity extends AppCompatActivity {
 
     TopicAdapter adapter;
     RecyclerView listElements;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
         listElements=(RecyclerView) findViewById(R.id.recycler_library);
-
-
-        GridLayoutManager llm = new GridLayoutManager(this, 2);
-        //LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(GridLayoutManager.VERTICAL);
+        listElements.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
         listElements.setLayoutManager(llm);
-
         adapter = new TopicAdapter(this);
         listElements.setAdapter(adapter);
-
 
     }
     @Override
