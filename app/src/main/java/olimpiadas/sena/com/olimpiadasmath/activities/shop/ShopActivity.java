6e75cbfd7cdf117730dfe9c527e3825fe59be516
rich4rd <rich4rdruiz@gmail.com>
2017-09-14 20:28:47 -0500

    Cambio en el shop con (Avatars e items ) 14 Septiembre 2017  8:28pm

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/shop/ShopActivity.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/shop/ShopActivity.java
@@ -3,9 +3,10 @@ package olimpiadas.sena.com.olimpiadasmath.activities.shop;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -14,36 +15,34 @@ import java.util.List;
 import olimpiadas.sena.com.olimpiadasmath.R;
 import olimpiadas.sena.com.olimpiadasmath.activities.menu.MainActivity;
 import olimpiadas.sena.com.olimpiadasmath.adapter.shop.ProductAdapter;
+import olimpiadas.sena.com.olimpiadasmath.adapter.shop.ViewPagerShopAdapter;
+import olimpiadas.sena.com.olimpiadasmath.fragments.shop.AvatarShopFragment;
 import olimpiadas.sena.com.olimpiadasmath.fragments.header.HeaderFragment;
+import olimpiadas.sena.com.olimpiadasmath.fragments.shop.ItemShopFragment;
 import olimpiadas.sena.com.olimpiadasmath.model.Product;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class ShopActivity extends AppCompatActivity {
+public class ShopActivity extends AppCompatActivity implements AvatarShopFragment.OnFragmentInteractionListener, ItemShopFragment.OnFragmentInteractionListener{
 
     RecyclerView recyclerViewShop;
     ProductAdapter adapter;
     List<Product> list;
     public HeaderFragment headerFragment;
 
+    ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
         getSupportActionBar().hide();
-        recyclerViewShop = (RecyclerView) findViewById(R.id.recycler_shop);
+
 
         headerFragment = (HeaderFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_shop_header);
 
+        viewPager = (ViewPager) findViewById(R.id.vpPagerShop);
+        viewPager.setAdapter(new ViewPagerShopAdapter(getSupportFragmentManager()));
 
-        // Adaptacion del recycler con el item shop
-
-        GridLayoutManager llm = new GridLayoutManager(this, 2);
-        //LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(GridLayoutManager.VERTICAL);
-        recyclerViewShop.setLayoutManager(llm);
-        list = llenarLista();
-        adapter = new ProductAdapter(list,this);
-        recyclerViewShop.setAdapter(adapter);
     }
 
     public static List<Product> llenarLista(){
@@ -66,5 +65,10 @@ public class ShopActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         startActivity(new Intent(ShopActivity.this, MainActivity.class));
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
