1e988dc8a9056b470797239fbb9a053da86346b5
rich4rd <rich4rdruiz@gmail.com>
2017-09-14 20:42:44 -0500

    Fixed Bug  Launch 14 Septiembre 2017  8:28pm

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/fragments/header/HeaderFragment.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/fragments/header/HeaderFragment.java
@@ -101,7 +101,7 @@ public class HeaderFragment extends Fragment {
 
 
         tvName.setText(appControl.currentUser.getNickname());
-        tvTickets.setText(" x " + appControl.currentUser.getTickets());
+//        tvTickets.setText(" x " + appControl.currentUser.getTickets());
         tvCoins.setText(" x " + appControl.currentUser.getCoins());
         tvExp.setText(" x " + ((int)appControl.currentUser.getExperience()));
         Log.d(TAG,"Antes de recibir la imagen");
