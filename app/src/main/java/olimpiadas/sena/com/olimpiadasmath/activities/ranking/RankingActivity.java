87f70ecebc1846f34690325611b33e2ef52c2a9c
Didier <deramirez19@misena.edu.co>
2017-10-08 15:25:19 -0500

    botones back

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/ranking/RankingActivity.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/ranking/RankingActivity.java
@@ -1,40 +1,40 @@
 package olimpiadas.sena.com.olimpiadasmath.activities.ranking;
 
 import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-
-
-import java.util.List;
+import android.widget.ImageButton;
 
 import olimpiadas.sena.com.olimpiadasmath.R;
-import olimpiadas.sena.com.olimpiadasmath.adapter.profile.ViewPagerAdapter;
-import olimpiadas.sena.com.olimpiadasmath.adapter.ranking.RankingAdapter;
-
+import olimpiadas.sena.com.olimpiadasmath.activities.menu.MainActivity;
 import olimpiadas.sena.com.olimpiadasmath.adapter.ranking.ViewPagerRankingAdapter;
-import olimpiadas.sena.com.olimpiadasmath.fragments.challenge.QuestionFragment;
-import olimpiadas.sena.com.olimpiadasmath.fragments.ranking.CoinsRankingFragment;
-import olimpiadas.sena.com.olimpiadasmath.fragments.ranking.GeneralRankingFragment;
-import olimpiadas.sena.com.olimpiadasmath.fragments.ranking.TimeRankingFragment;
-import olimpiadas.sena.com.olimpiadasmath.model.User;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class RankingActivity extends AppCompatActivity {
 
     ViewPager viewPager;
+    ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
         getSupportActionBar().hide();
+
         viewPager = (ViewPager) findViewById(R.id.vpPagerRanking);
         viewPager.setAdapter(new ViewPagerRankingAdapter(getSupportFragmentManager()));
+
+        btnBack = (ImageButton) findViewById(R.id.btn_back_ranking);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(RankingActivity.this, MainActivity.class);
+                startActivity(goBack);
+            }
+        });
     }
 
     @Override
