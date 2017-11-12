08fef4a05433da7c74415ac18391206f2c096448
andresmunozl <jefferson@unicauca.edu.co>
2017-11-12 12:10:52 -0500

    puliendo detalles de diseño dela app

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/util/webConManager/WebConnectionManager.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/util/webConManager/WebConnectionManager.java
@@ -26,7 +26,7 @@ public class WebConnectionManager implements WebConnection.WebConnectionListener
 
 
     //private final String url = "http://10.73.70.29:8097/";
-    private final String url = "http://10.73.120.153:8097/";
+    private final String url = "http://192.168.0.13:8097/";
     //private final String url = "http://192.168.43.14:8097/";
     //private final String url = "http://192.168.137.1:8097/";
 
