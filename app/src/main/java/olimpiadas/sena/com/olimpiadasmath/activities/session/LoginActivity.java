4bb7f7947130f268ef969329d507136184d25be5
rich4rd <rich4rdruiz@gmail.com>
2017-11-12 14:50:15 -0500

    Arreglo de las 5 preguntas en la practica

--- app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/session/LoginActivity.java
+++ app/src/main/java/olimpiadas/sena/com/olimpiadasmath/activities/session/LoginActivity.java
@@ -12,7 +12,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
